8c02572f491f7eda14996ff04e9586a986ef6b9a
Samikshya <samikshyaaryal5@gmail.com>
2020-03-10 12:31:22 +0200

    using promises to get the images

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    async onSearchSubmit(term){
+        const response = await  axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID 6BTte4m6KE2fsWRArcLJdIzplfedg6GrBlxes1T5Esg'
+            }
+
+        });
+        console.log(response.data.results);
     }
     render(){
         return (
-            <div className="ui-container" style={{marginTop: '10px'}}>
+            <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
             </div>
         )
